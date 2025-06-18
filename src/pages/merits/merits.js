e78bdc10b8821daf25417da7a26d84d6bf55d20a
MaleficiumT <timonina.maria.356@mail.ru>
2025-06-18 20:14:21 +0300

    5

---
+++ src/pages/merits/merits.js
@@ -0,0 +1,1 @@
+import './merits.css'
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'src/pages/about/about.html'),
-        services: resolve(__dirname, 'src/pages/services/services.html')
+        services: resolve(__dirname, 'src/pages/services/services.html'),
+				merits: resolve(__dirname, 'src/pages/merits/merits.html'),
+				studios: resolve(__dirname, 'src/pages/studios/studios.html')
         // добавляй другие страницы сюда
       }
     }
